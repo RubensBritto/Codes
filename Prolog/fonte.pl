18193ceb6573b7c55101dea2426dfc32152fad12
José Rubens <47110177+RubensBritto@users.noreply.github.com>
2019-03-08 20:37:56 -0300

    Update fonte.pl

--- Prolog/fonte.pl
+++ Prolog/fonte.pl
@@ -1,4 +1,7 @@
-fonte(V, [], E):- E > 0.
+fonte(_, [], E):- E > 0.
 fonte(V, [(V,H)|T], E):- H =\= V, E1 is E + 1,fonte(V, T, E1).
 
 fonte(V, [(H, I)|T], E):- H =\= V, I =\= V, fonte(V, T, E).
+
+
+:- fonte(1,[(1,2),(1,3),(2,3)],0).
